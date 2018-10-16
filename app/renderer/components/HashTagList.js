38d6db97c863d86ad96a8a1d7baf0d86998f6cd9
Jacob Kittley-Davies <jacob@kittley.com>
2018-10-16 12:01:16 +0100

    Updated colour scheme

--- app/renderer/components/HashTagList.js
+++ app/renderer/components/HashTagList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ListGroupItem, FormGroup, Label, Input, ListGroup } from 'reactstrap';
 
 export const HashtagItem = (props) => {
-  return <ListGroupItem  style={{ backgroundColor: "#47484c", color: '#fff' }}>
+  return <ListGroupItem style={{ backgroundColor: "#47484c", color: '#fff' }}>
   <FormGroup check>
     <Label check>
       <Input type="checkbox" onChange={ () => props.onCheckChange(props.name) } checked={props.checked} />
@@ -15,7 +15,7 @@ export const HashtagItem = (props) => {
 export const HashTagList = (props) => {
 
   if (Object.keys(props.tags).length === 0) return <ListGroup flush>
-  <ListGroupItem  style={{ backgroundColor: "#eee"}}>No Hashtags detected</ListGroupItem>
+  <ListGroupItem style={{ backgroundColor: "#47484c", color: '#fff' }}>No Hashtags detected</ListGroupItem>
   </ListGroup>;
 
   return <ListGroup flush>
