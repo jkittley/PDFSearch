97e6c89f75ebf511c1378b1b540dceb1de580ba0
Jacob Kittley-Davies <jacob@kittley.com>
2018-10-16 12:01:04 +0100

    Redesigned layout

--- app/renderer/components/About.js
+++ app/renderer/components/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Button, Row, Col, Card, CardImg, CardBody, CardText, CardTitle, CardSubtitle } from 'reactstrap'
-
+import shell from 'electron';
 
 export default class About extends Component {
 
@@ -10,27 +10,30 @@ export default class About extends Component {
 
   render() {
     return (
-
       <div className="container text-center">
         <Button close onClick={ ()=> this.handleExit() }/>
         <h2 className="m-4">About</h2>
         <Row>
-          <Col sm={{ size: 6, offset: 3 }}>
-          <Card>
+          <Col sm={ styles.frame }>
+          <Card className="border-0">
           <CardBody>
-            <CardSubtitle>This application was developed and is supported by Kittley Ltd</CardSubtitle>
-            <CardImg top src="assets/kittley-logo.png" alt="Kittley Limited Logo" />
+            <CardText>
+              This project is Open Source and all contributions to the code base are welcome. See https://github.com/jkittley/PDFSearch for more details.
+              The maintenance of this project is supported by Kittley Ltd (www.kittley.com).
+            </CardText>
+
+            <CardImg top src="assets/kittley-logo.png" alt="Kittley Limited Logo" className="mt-4" />
 
             <CardText className="mt-4"></CardText>
           </CardBody>
         </Card>
           </Col>
         </Row>
-
-
-
-
       </div>
     );
   }
+}
+
+const styles = {
+  frame: { size: 8, offset: 2 },
 }
